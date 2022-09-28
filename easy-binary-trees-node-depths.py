ba2e1b8b178b42fa07752c9f4343ea51ad9ef83e
Michael Markieta <markieta.m@gmail.com>
2022-09-28 13:26:33 -0400

    Create easy-binary-trees-node-depths.py

---
+++ easy-binary-trees-node-depths.py
@@ -0,0 +1,38 @@
+# Solution 1
+# def nodeDepths(root):
+#     depths = []
+#     dfs(root, -1, depths)
+#     return sum(depths)
+
+# def dfs(node, depth, depths):
+#     depth += 1
+    
+#     if node is None:
+#         return
+
+#     depths.append(depth)
+    
+#     dfs(node.left, depth, depths)
+#     dfs(node.right, depth, depths)
+    
+
+# # This is the class of the input binary tree.
+# class BinaryTree:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+
+# Solution 2
+def nodeDepths(root, depth = 0):
+    if root is None:
+        return 0
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
