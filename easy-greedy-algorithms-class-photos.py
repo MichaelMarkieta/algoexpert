93675d59d9a6572975b4a53ce7ffe207989ae926
Michael Markieta <markieta.m@gmail.com>
2022-09-28 13:28:36 -0400

    Create easy-greedy-algorithms-class-photos.py

---
+++ easy-greedy-algorithms-class-photos.py
@@ -0,0 +1,17 @@
+# Solution 1
+def classPhotos(redShirtHeights, blueShirtHeights):
+    redShirtHeights.sort()
+    blueShirtHeights.sort()
+    
+    redTaller = True if redShirtHeights[0] > blueShirtHeights[0] else False
+
+    if redTaller:
+        for i, x in enumerate(redShirtHeights):
+            if x <= blueShirtHeights[i]:
+                return False
+    else:
+        for i, x in enumerate(blueShirtHeights):
+            if x <= redShirtHeights[i]:
+                return False
+    
+    return True
